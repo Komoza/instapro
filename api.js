ca25fa0453a1780d5f4bda9c6053e3a17bd4d11c
Maxim <MaximKomoza@yandex.ru>
2023-05-12 10:37:14 +0700

    render posts via api

--- api.js
+++ api.js
@@ -2,6 +2,7 @@
 // "боевая" версия инстапро лежит в ключе prod
 const personalKey = "prod";
 const baseHost = "https://webdev-hw-api.vercel.app";
+
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
 export function getPosts({ token }) {
