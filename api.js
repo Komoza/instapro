54b12f8bd181c5f35bb2159924e704d80d2dc573
Maxim <MaximKomoza@yandex.ru>
2023-05-14 19:27:16 +0700

    fix image size + adaptive

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
 const personalKey = "Komoza-dev";
+// const personalKey = "prod";
+
+
+
 const baseHost = "https://webdev-hw-api.vercel.app";
 
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
